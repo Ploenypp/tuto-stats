e0921cf27053303e2feeee489fcc0e2607503b65
Ploenypp <everhonorlight@gmail.com>
2025-04-02 01:47:32 +0200

    init hs_stats

---
+++ hs_stats.py
@@ -0,0 +1,46 @@
+from math import *
+from collections import Counter
+import numpy as np
+
+def mean(data) :
+    return fsum(data)/len(data)
+
+def median(data) :
+    i, j = 0, len(data)-1
+
+    if len(data)%2 == 0 :
+        while i != j-1 :
+            i += 1
+            j -= 1
+        return (data[i]+data[j])/2
+
+    else :
+        while i != j :
+            i += 1
+            j -= 1
+        return data[i]
+    
+def mode(data) :
+    hashtable = dict(Counter(data).most_common())
+    
+    modes = set()
+    val = -1
+
+    for x,freq in hashtable.items() :
+        if freq > val :
+            modes = {x}
+            val = freq
+        if freq == val :
+            modes.add(x)
+    
+    return modes
+
+def p_variance(data) :
+    m = mean(data)
+    sum = fsum([(x - m)**2 for x in data])
+    return sum/len(data)
+
+def s_variance(data) :
+    m = mean(data)
+    sum = fsum([(x - m)**2 for x in data])
+    return sum/(len(data)-1)
