599b16f718ed1e8d78a62bccf9258b6999cf872a
Ploenypp <everhonorlight@gmail.com>
2025-07-07 14:07:36 +0700

    moving and updating strat_game files

--- strat_game/strat_game.py
+++ strat_game/strat_game.py
@@ -28,8 +28,11 @@ class Board :
             print("")
 
     def place_block(self, player, x, y) :
-        if x <= 0 or x >= self.dim - 1 or y <= 0 or y >= self.dim - 1 : # coordinate check
-            print("(!) invalid coordinates")
+        if x < 0 or x > self.dim - 1 or y < 0 or y > self.dim - 1 : # coordinate check
+            print("\n\t(!) invalid coordinates")
+            return False
+        if (x == 0 and y == 0) or (x == self.dim -1 and y == self.dim-1): # coordinate check
+            print("\n\t(!) invalid coordinates")
             return False
         b = 0
         if player == 1 :
@@ -37,7 +40,7 @@ class Board :
                 if self.p1_blocks[b] == -1 : break
                 b += 1
             if b == self.dim :
-                print("(!) no blocks available")
+                print("\n\t(!) no blocks available")
                 return False
             if self.grid[y][x] == " " : 
                 self.grid[y][x] = "☐"
@@ -48,7 +51,7 @@ class Board :
                 if self.p2_blocks[b] == -1 : break
                 b += 1
             if b == self.dim : 
-                print("(!) no blocks available")
+                print("\n\t(!) no blocks available")
                 return False
             if self.grid[y][x] == " " : 
                 self.grid[y][x] = "◼︎"
@@ -59,7 +62,7 @@ class Board :
     def move_player(self, player, x, y) :
         # coordinate check by player class
         if self.grid[y][x] != " " :
-            print("(!) inaccessible square")
+            print("\n\t(!) inaccessible square")
             return False
         if player == 1 :
             self.grid[self.p1[1]][self.p1[0]] = " "
@@ -81,8 +84,20 @@ class Board :
             print("Player 2 wins!")
             return True
         # countdown blocks
-        for x in self.p1_blocks : 
-            if x > -1 : x -= 1
+        for i in range(dim-1) :
+            if self.p1_blocks[i] > -1 :
+                self.p1_blocks[i] -= 1
+                if self.p1_blocks[i] == 0 :
+                    self.p1_blocks[i] = -1
+                    self.p1.blocks += 1
+
+            if self.p2_blocks[i] > -1 :
+                self.p2_blocks[i] -= 1
+                if self.p2_blocks[i] == 0 :
+                    self.p2_blocks[i] = -1
+                    self.p2.blocks += 1
+
+        
         
         return False
 
@@ -99,31 +114,33 @@ class Player :
         match dir :
             case "a" :
                 if self.x == 0 : # left
-                    print("(!) invalid move -- ",end="")
+                    print("\n\t(!) invalid move")
                     return False
                 if board.move_player(self.id, self.x - 1, self.y) : self.x -= 1
             case "w" :
                 if self.y == 0 : # up
-                    print("(!) invalid move -- ",end="")
+                    print("\n\t(!) invalid move")
                     return False
                 if board.move_player(self.id, self.x, self.y - 1) : self.y -= 1
             case "s" : # down
                 if self.y == self.dim - 1 :
-                    print("(!) invalid move -- ",end="")
+                    print("\n\t(!) invalid move")
                     return False
                 if board.move_player(self.id, self.x, self.y + 1) : self.y += 1
             case "d" : # right
                 if self.x == self.dim - 1 :
-                    print("(!) invalid move -- ",end="")
+                    print("\n\t(!) invalid move")
                     return False
                 if board.move_player(self.id, self.x + 1, self.y) : self.x += 1
         return True
     
     def place_block(self, x, y, board) :
         if self.blocks == 0 : 
-            print("no blocks available")
+            print("\n\t(!) no blocks available")
             return False
-        if board.place_block(self.id, x, y) : self.blocks -= 1
+        if board.place_block(self.id, x, y) : 
+            self.blocks -= 1
+            return True
 
 class CompletelyRandomBot :
     def __init__(self, player_class:Player, board:Board, dim) :
@@ -133,15 +150,21 @@ class CompletelyRandomBot :
 
     def play(self) :
         success = False
-        if random() < 0.5 : # move
+        if self.p.blocks != 0 :
+            if random() < 0.5 : # move
+                moves = ["a","s","w","d"]
+                success = self.p.move_player(choice(moves),self.b)
+                while not success :
+                    success = self.p.move_player(choice(moves),self.b)
+            else : # place block
+                success = self.p.place_block(randint(0,self.dim-1), randint(0,self.dim-1), self.b)
+                while not success :
+                    success = self.p.place_block(randint(0,self.dim-1), randint(0,self.dim-1), self.b)
+        else : 
             moves = ["a","s","w","d"]
             success = self.p.move_player(choice(moves),self.b)
             while not success :
                 success = self.p.move_player(choice(moves),self.b)
-        else : # place block
-            success = self.p.place_block(randint(0,self.dim-1), randint(0,self.dim-1))
-            while not success :
-                success = self.p.place_block(randint(0,self.dim-1), randint(0,self.dim-1))
 
 # initialization
 dim = 10
@@ -153,6 +176,7 @@ b.show()
 
 game_over = False
 while not game_over :
+    print("\n# YOUR TURN")
     op_success = False
     op = int(input("[1] move player / [2] place block --"))
     while op != 1 and op != 2 : 
@@ -160,22 +184,23 @@ while not game_over :
         
     if op == 1 :
         while not op_success :
-            dir = input("input direction\n [a] : left / [w] : up / [s] : down / [d] : right --")
+            dir = input("\tinput direction -> [a] : left / [w] : up / [s] : down / [d] : right --")
             while dir != "a" and dir != "w" and dir != "s" and dir != "d" :
                 print(dir != "a" and dir != "w" and dir != "s" and dir != "d")
-                dir = input("input direction\n [a] : left / [w] : up / [s] : down / [d] : right --")
+                dir = input("\tinput direction -> [a] : left / [w] : up / [s] : down / [d] : right --")
             op_success = p1.move_player(dir, b) 
         
     if op == 2 :
         while not op_success :
-            x = int(input(f"input x-coordinate (0 : {dim - 1})"))
+            x = int(input(f"input x-coordinate (0 : {dim - 1}) --"))
             while x < 0 or x > dim - 1 : 
-                x = int(input(f"input x-coordinate (0 : {dim - 1})"))
+                x = int(input(f"input x-coordinate (0 : {dim - 1}) --"))
                 
-            y = int(input(f"input y-coordinate (0 : {dim - 1})"))
+            y = int(input(f"input y-coordinate (0 : {dim - 1}) --"))
             while y < 0 or y > dim - 1 : 
-                y = int(input(f"input y-coordinate (0 : {dim - 1})"))
+                y = int(input(f"input y-coordinate (0 : {dim - 1}) --"))
             op_success = p1.place_block(x, y, b)
+    print("\n#BOT'S TURN")
     bot.play()
     b.show()
     game_over = b.update()
